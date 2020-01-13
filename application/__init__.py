651a0882876ee8d17cd1c396a2348f8823c6ca10
LoganAnde <posttrauma.logan@gmail.com>
2020-01-13 20:46:26 +0000

    adding initial files and directories

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+import json
+import urllib3
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,16 @@
+from flask import render_template
+from flask import send_from_directory
 from application import app
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Logan'}
+    return render_template('index.html', title = 'Home', user = user)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
